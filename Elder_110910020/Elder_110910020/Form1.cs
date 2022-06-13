693f5f3877e6c7a60b5e7a670b82b7487f165064
pinguinjay <83256594+pinguinjay@users.noreply.github.com>
2022-06-13 15:50:59 +0800

    Add files via upload

---
+++ Elder_110910020/Elder_110910020/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Elder_110910020
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tableBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.tableBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.database_PersonInfoDataSet1);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: 這行程式碼會將資料載入 'database_PersonInfoDataSet1.Table' 資料表。您可以視需要進行移動或移除。
+            this.tableTableAdapter.Fill(this.database_PersonInfoDataSet1.Table);
+
+        }
+    }
+}
