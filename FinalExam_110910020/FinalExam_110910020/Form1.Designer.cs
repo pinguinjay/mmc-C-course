f5b8e354023e580bfd1a8b6c0fff3f6ecccb37ef
pinguinjay <83256594+pinguinjay@users.noreply.github.com>
2022-06-20 11:22:57 +0800

    Add files via upload

---
+++ FinalExam_110910020/FinalExam_110910020/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace FinalExam_110910020
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radioButton_outerEar = new System.Windows.Forms.RadioButton();
+            this.radioButton_middleear = new System.Windows.Forms.RadioButton();
+            this.radioButton_innerEar = new System.Windows.Forms.RadioButton();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radioButton_outerEar
+            // 
+            this.radioButton_outerEar.AutoSize = true;
+            this.radioButton_outerEar.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.radioButton_outerEar.Location = new System.Drawing.Point(13, 26);
+            this.radioButton_outerEar.Name = "radioButton_outerEar";
+            this.radioButton_outerEar.Size = new System.Drawing.Size(60, 23);
+            this.radioButton_outerEar.TabIndex = 0;
+            this.radioButton_outerEar.TabStop = true;
+            this.radioButton_outerEar.Text = "外耳";
+            this.radioButton_outerEar.UseVisualStyleBackColor = true;
+            this.radioButton_outerEar.Click += new System.EventHandler(this.radioButton_outerEar_Click);
+            // 
+            // radioButton_middleear
+            // 
+            this.radioButton_middleear.AutoSize = true;
+            this.radioButton_middleear.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.radioButton_middleear.Location = new System.Drawing.Point(90, 26);
+            this.radioButton_middleear.Name = "radioButton_middleear";
+            this.radioButton_middleear.Size = new System.Drawing.Size(60, 23);
+            this.radioButton_middleear.TabIndex = 1;
+            this.radioButton_middleear.TabStop = true;
+            this.radioButton_middleear.Text = "中耳";
+            this.radioButton_middleear.UseVisualStyleBackColor = true;
+            this.radioButton_middleear.Click += new System.EventHandler(this.radioButton_middleear_Click);
+            // 
+            // radioButton_innerEar
+            // 
+            this.radioButton_innerEar.AutoSize = true;
+            this.radioButton_innerEar.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.radioButton_innerEar.Location = new System.Drawing.Point(168, 26);
+            this.radioButton_innerEar.Name = "radioButton_innerEar";
+            this.radioButton_innerEar.Size = new System.Drawing.Size(60, 23);
+            this.radioButton_innerEar.TabIndex = 2;
+            this.radioButton_innerEar.TabStop = true;
+            this.radioButton_innerEar.Text = "內耳";
+            this.radioButton_innerEar.UseVisualStyleBackColor = true;
+            this.radioButton_innerEar.Click += new System.EventHandler(this.radioButton_innerEar_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 19;
+            this.listBox1.Location = new System.Drawing.Point(13, 81);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(227, 194);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(12, 323);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 19);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "功能解釋";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(13, 359);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(227, 156);
+            this.textBox1.TabIndex = 5;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(335, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(466, 379);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList1.ImageStream")));
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList1.Images.SetKeyName(0, "outerEar.png");
+            this.imageList1.Images.SetKeyName(1, "MiddleEar.png");
+            this.imageList1.Images.SetKeyName(2, "innerEar.png");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 593);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.radioButton_innerEar);
+            this.Controls.Add(this.radioButton_middleear);
+            this.Controls.Add(this.radioButton_outerEar);
+            this.Name = "Form1";
+            this.Text = "耳朵導覽_110910020";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton_outerEar;
+        private System.Windows.Forms.RadioButton radioButton_middleear;
+        private System.Windows.Forms.RadioButton radioButton_innerEar;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ImageList imageList1;
+    }
+}
+
---
+++ FinalExam_110910020/FinalExam_110910020/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalExam_110910020
+{
+    public partial class Form1 : Form
+    {
+        string[] OuterEar = {"耳輪","對耳輪","耳膜","外耳道","耳珠" };
+        string[] OuterEarDescription = {
+            "外耳頂部周圍的彎曲邊緣",
+            "與耳輪相對，上部有分叉的隆起部分",
+            "耳膜也稱鼓膜，為一彈性灰白色半透明薄膜，將外耳道與中耳隔開",
+            "外耳道是自耳甲腔深處的外耳門向內側延伸到鼓膜的彎曲管道，可把耳郭收集的聲波傳導至鼓膜，有阻止異物進入外耳道侵害鼓膜的作用",
+            "耳珠為耳朵最下面的部分，沒特別作用，傳統上認為越大越有福氣"
+        };
+        string[] MiddleEar = { "鼓張肌", "錘骨", "砧骨", "鐙骨", "耳道","鼓膜", "鐙骨肌" ,"鼓室","耳咽管","內耳迷路"};
+        string[] MiddleEarDescription = {
+            "鼓張肌是中耳內的一塊肌肉，位於聽管骨部分上方的骨管中，並連接到錘骨",
+            "是人耳中錘狀的小骨，是三塊聽小骨（ossicles）中的一個，連接耳膜和砧骨",
+            "砧骨（耳內三小骨之一，將聲音從耳膜傳至內耳）",
+            "人類和其他動物的中耳骨骼，參與將聲音振動傳導至內耳",
+            "耳道可把耳郭收集的聲波傳導至鼓膜，有阻止異物進入外耳道侵害鼓膜的作用",
+            "又稱耳膜（eardrum），是分割外耳和中耳的薄膜，將耳道和鼓室隔開[1][2][3]。鼓膜是耳的重要組成部分，它獲取空氣中的聲音，並將之傳遞給中耳中的聽小骨",
+            "維持卵圓窗張力限制卵圓窗過度震動",
+            "是位於中耳內的一個空心小腔室，由骨質結構包圍。鼓室位於鼓膜後，內部有三塊聽小骨",
+            "耳咽管或稱聽管、咽鼓管、歐氏管（英語：Eustachian tube、E－tube或 auditory tube），是連接咽喉和中耳的管道",
+            "分為骨迷路和膜迷路。骨迷路內含有液體，外淋巴存在於骨迷路和膜迷路之間、起保護作用，而內淋巴存在於膜迷路內"
+        };
+        string[] innerEar = { "上半規管", "後半規管", "側半規管", "前庭", "耳蝸","橢圓囊","球狀囊" };
+        string[] innerEarDescription = {
+            "上半規管或前半規管是前庭系統的一部分，可檢測頭部圍繞側軸的旋轉，即在矢狀面上的旋轉。 例如，在點頭時會發生這種情況。",
+            "後半規管是前庭系統的一部分，它檢測頭部圍繞前後（矢狀）軸的旋轉，或者換句話說，在冠狀平面上的旋轉。 例如，當一個人移動頭部以接觸肩膀時，或者在做側手翻時，就會發生這種情況。",
+            "側半規管是三個管中最短的。 該管內的流體運動對應於頭部圍繞垂直軸的旋轉",
+            "是內耳骨迷路的一部分，位於半規管和耳蝸之間，與半規管共同構成前庭系統。前庭內含有球囊（saccule）、橢圓囊（utricle），屬於平衡覺感受器，能感知直線加速運動",
+            "耳蝸的核心部分為柯蒂氏器，是聽覺傳導器官，負責將來自中耳的聲音信號轉換為相應的神經電信號，交送腦的中樞聽覺系統接受進一步處理，最終實現聽覺",
+            "橢圓囊包含稱為毛細胞的機械感受器，可以區分頭部傾斜的程度，這要歸功於它們的頂端靜纖毛設置。 這些被耳石覆蓋，由於重力，耳石會拉動靜纖毛並使其傾斜。",
+            "球囊是內耳中的感覺細胞床。 它將頭部運動轉化為神經衝動，供大腦解釋。",
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton_outerEar_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            pictureBox1.Image = imageList1.Images[0];
+            for (int i = 0; i <= OuterEar.GetUpperBound(0); i++)
+            {
+                listBox1.Items.Add(OuterEar[i]);
+            }
+        }
+
+        private void radioButton_middleear_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            pictureBox1.Image = imageList1.Images[1];
+            for (int i = 0; i <= MiddleEar.GetUpperBound(0); i++)
+            {
+                listBox1.Items.Add(MiddleEar[i]);
+            }
+        }
+
+        private void radioButton_innerEar_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            pictureBox1.Image = imageList1.Images[2];
+            for (int i = 0; i <= innerEar.GetUpperBound(0); i++)
+            {
+                listBox1.Items.Add(innerEar[i]);
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (radioButton_outerEar.Checked == true)
+            {
+                textBox1.Text = OuterEarDescription[listBox1.SelectedIndex];
+            }
+            else if(radioButton_middleear.Checked == true)
+            {
+                textBox1.Text = MiddleEarDescription[listBox1.SelectedIndex];
+            }
+            else
+            {
+                textBox1.Text = innerEarDescription[listBox1.SelectedIndex];
+            }
+        }
+    }
+}
